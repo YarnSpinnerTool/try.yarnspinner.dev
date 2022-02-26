d8463bd964695b72914128a4d181c86a279c6b01
Jon Manning <jon@secretlab.com.au>
2022-02-26 18:38:17 +1100

    In production: Compress and rename .js to .js.br

--- webpack.config.js
+++ webpack.config.js
@@ -2,67 +2,82 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin');
 const CompressionWebpackPlugin = require('compression-webpack-plugin');
+const HtmlWebpackChangeAssetsExtensionPlugin = require('html-webpack-change-assets-extension-plugin')
 
 const zlib = require("zlib");
 
-module.exports = {
-    entry: {
-        index: './src/index.ts',
-        playground: './src/playground.ts',
-    },
-    output: {
-        filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-        clean: true,
-    },
-    resolve: {
-        extensions: ['.ts', '.js', '.json', '.css', '.ttf']
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: 'Development',
-            template: 'src/index.html'
-        }),
-        new MonacoWebpackPlugin({
-            languages: ["markdown"],
-        }),
-        new CompressionWebpackPlugin({
-            filename: "[path][base].br",
-            algorithm: "brotliCompress",
-            test: /\.(js|css|html|svg)$/,
-            compressionOptions: {
-              params: {
-                [zlib.constants.BROTLI_PARAM_QUALITY]: 11,
-              },
-            },
-            threshold: 10240,
-            minRatio: 0.8,
-            deleteOriginalAssets: (process.env['DELETE_ORIGINAL_ASSETS'] || 0) ? true : false,
-          }),
-    ],
-    devServer: {
-        static: './dist',
-        watchFiles: ['src/**/*.html', 'bin/**'],
-    },
-    devtool: 'source-map',
-    mode: 'development',
 
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader'],
-            },
-            {
-                test: /\.tsx?$/,
-                use: 'ts-loader',
-                exclude: /node_modules/,
-            },
-        ]
-    },
-    optimization: {
-        splitChunks: {
-            chunks: 'all',
+module.exports = (env, argv) => {
+    const isProduction = argv.mode === "production";
+
+    return {
+        entry: {
+            index: './src/index.ts',
+            playground: './src/playground.ts',
         },
-    }
+        output: {
+            filename: '[name].bundle.js',
+            path: path.resolve(__dirname, 'dist'),
+            clean: true,
+        },
+        resolve: {
+            extensions: ['.ts', '.js', '.json', '.css', '.ttf']
+        },
+        plugins: [
+
+            new HtmlWebpackPlugin({
+                title: 'Development',
+                template: 'src/index.html',
+                jsExtension: '.br'
+            }),
+            new MonacoWebpackPlugin({
+                languages: ["markdown"],
+            }),
+
+            ...(isProduction ? [ 
+                new CompressionWebpackPlugin({
+                    filename: "[path][base].br",
+                    algorithm: "brotliCompress",
+                    test: /\.(js|css|svg)$/,
+                    compressionOptions: {
+                        params: {
+                            [zlib.constants.BROTLI_PARAM_QUALITY]: 11,
+                        },
+                    },
+                    // threshold: 10240,
+                    threshold: 0,
+                    // minRatio: 0.8,
+                    minRatio: Infinity,
+                    deleteOriginalAssets: (process.env['DELETE_ORIGINAL_ASSETS'] || 0) ? true : false,
+                }),
+                new HtmlWebpackChangeAssetsExtensionPlugin(),
+            ] : []
+            )
+        ],
+        devServer: {
+            static: './dist',
+            watchFiles: ['src/**/*.html', 'bin/**'],
+        },
+        devtool: 'source-map',
+        mode: 'development',
+
+        module: {
+            rules: [
+                {
+                    test: /\.css$/i,
+                    use: ['style-loader', 'css-loader'],
+                },
+                {
+                    test: /\.tsx?$/,
+                    use: 'ts-loader',
+                    exclude: /node_modules/,
+                },
+            ]
+        },
+        optimization: {
+            splitChunks: {
+                chunks: 'all',
+            },
+        }
+    };
 };

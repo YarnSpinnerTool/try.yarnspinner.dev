d7e57f1d835436ca5414f4d4751f210ed6f229cb
Jon Manning <jon@secretlab.com.au>
2022-08-05 23:18:21 +1000

    Tidy up and add comments

--- src/index.ts
+++ src/index.ts
@@ -2,20 +2,22 @@ import  { initialContent } from './starter-content'
 
 window.addEventListener('load', async function () {
 
+    // First, determine what content we want to load. If the url contains a
+    // hash, and that hash matches a key inside the initialContent data, then we
+    // want to laod that content.
     let location = window.location.href;
     let url = new URL(location);
     let hashComponents = url.hash.replace(/^#/, "").split('/')
 
     let contentName : string | undefined
     
-    if (url.hash.length > 0) {
-        if (initialContent[hashComponents[0]]) {
-            contentName = hashComponents[0]
-        }
+    if (url.hash.length > 0 && initialContent[hashComponents[0]]) {
+        contentName = hashComponents[0]
     }
     
     // Wait for the playground module to finish being downloaded, and then
-    // import it. Once that's done, load the playground.
+    // import it. Once that's done, load the playground with the content that we
+    // selected.
     const playground = await import("./playground");
     await playground.load(contentName);
     

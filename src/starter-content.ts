ac353926c9a51259bd4b71fea98d44362885c350
Jon Manning <jon@secretlab.com.au>
2024-03-27 00:09:54 +1100

    Set default content to WIP pirate story

--- src/starter-content.ts
+++ src/starter-content.ts
@@ -1,4 +1,5 @@
 export const initialContent: { [key: string]: string } = {
-    "default": require('./initial-content/Default.yarn'),
+    // "default": require('./initial-content/Default.yarn'),
     "pax": require('./initial-content/PAX2022.yarn'),
+    "default": require('./initial-content/Pirate.yarn'),
 }
